e8960e1648e57443c9c1b03e9f6ff1d8a9ae2fec
Mikhail Polykovskii <glader.ru@gmail.com>
2024-04-16 17:54:20 +0700

    Redirect url

--- payments/cloudpayments.py
+++ payments/cloudpayments.py
@@ -92,7 +92,7 @@ class CloudPaymentsService:
             "Description": product_data["description"],
             "RequireConfirmation": False,
             "InvoiceId": order_id,
-            "SuccessRedirectUrl": "https://club.carreerfactory.ru/intro/",
+            "SuccessRedirectUrl": "https://club.careerfactory.ru/intro/",
             "Email": user.email,
             "JsonData": {
                 "CloudPayments": {
@@ -110,7 +110,7 @@ class CloudPaymentsService:
                             },
                         ],
                         "email": user.email,
-                        "calculationPlace": "club.carreerfactory.ru",
+                        "calculationPlace": "club.careerfactory.ru",
                         "amounts":
                         {
                             "electronic": product_data["amount"],
