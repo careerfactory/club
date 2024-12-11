bd0489f156272ef674b556a27c0722dbb0c6b4a3
maskyy <62175286+maskyy@users.noreply.github.com>
2024-12-11 14:30:43 +0300

    Добавляет эндпоинт для получения клиентности юзера
    Требует сервисный токен god-юзера

--- users/api.py
+++ users/api.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse
+from django.http import HttpResponseBadRequest, JsonResponse
 from django.shortcuts import get_object_or_404
 
 from authn.decorators.api import api
@@ -28,3 +28,40 @@ def api_profile_by_telegram_id(request, telegram_id):
     return JsonResponse({
         "user": user.to_dict()
     })
+
+
+@api(require_auth=True)
+def api_profile_status(request):
+    if not request.me.is_god:
+        raise ApiAccessDenied(title="God only")
+
+    email = request.GET.get("email", None)
+    telegram_id = request.GET.get("telegram_id", None)
+    if email is None and telegram_id is None:
+        return HttpResponseBadRequest("Specify email and/or telegram_id")
+
+    telegram_user = User.objects.filter(telegram_id=telegram_id)
+    telegram_user = (
+        telegram_user[0] if telegram_user and telegram_id is not None else None
+    )
+    email_user = User.objects.filter(email=email)
+    email_user = email_user[0] if email_user and email is not None else None
+
+    active = None
+    club_telegram_id = None
+    club_email = None
+    if telegram_user is not None:
+        active = telegram_user.is_active_member
+        club_telegram_id = telegram_user.telegram_id
+        club_email = telegram_user.email
+    if email_user is not None:
+        active = active or email_user.is_active_member
+        club_telegram_id = club_telegram_id or email_user.telegram_id
+        club_email = club_email or email_user.email
+
+    body = {
+        "is_active_member": active,
+        "telegram_id": club_telegram_id,
+        "email": club_email,
+    }
+    return JsonResponse(body)
