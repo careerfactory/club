ad407445590fe7eb050b13188b85c440a71f8755
Timur <gub.timur@gmail.com>
2024-12-19 23:53:14 +0300

    Делаю базу опять доступной

--- posts/admin.py
+++ posts/admin.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 from posts.models.linked import LinkedPost
 from posts.models.post import Post
 
-
 class PostsAdmin(admin.ModelAdmin):
     list_display = (
         "slug",
